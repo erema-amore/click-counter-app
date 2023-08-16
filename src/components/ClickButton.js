f24a1ff850de0403fdb7b0dc0970b04bdba2baf4
erema-amore <erema.amore@mail.ru>
2023-08-16 15:57:39 +0600

    first commit

--- src/components/ClickButton.js
+++ src/components/ClickButton.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { increment, reset } from '../features/counterSlice';
-
+import './ClickButton.css'; 
 const ClickButton = () => {
   const dispatch = useDispatch();
+  const [clicked, setClicked] = useState(false); 
 
   const handleClick = () => {
     dispatch(increment());
+    setClicked(true); 
+    setTimeout(() => setClicked(false), 300); 
   };
 
   const handleReset = () => {
     dispatch(reset());
+    setClicked(true); 
+    setTimeout(() => setClicked(false), 300);
   };
 
   return (
-    <div className="button">
+    <div className={`button ${clicked ? 'clicked' : ''}`}>
       <button onClick={handleClick}>Click me</button>
       <button onClick={handleReset}>Reset</button>
     </div>
